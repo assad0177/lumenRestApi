198d1b982633d4fb2c69c1490877dcd2d6c40f20
Assad <assadtawakal98@gmail.com>
2022-01-28 17:22:34 +0500

    lumen rest api final code

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //App\Models\Post::factory()->count(17)->create()
+        // $this->call('UsersTableSeeder');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+
+});
+
+$router->get('/key', function() {
+    return \Illuminate\Support\Str::random(32);
+}); // to create a key for the project.
+    //alternate of artisan command : php artisan key:generate
+
+    $router->group(['prefix'=>'api'], function() use ($router){
+        $router->get('/posts','PostController@index');
+        $router->post('/posts','PostController@store');
+        $router->post('/posts/{id}','PostController@update');
+        $router->delete('/posts/{id}','PostController@destroy');
+    });
+
+    
+
+
